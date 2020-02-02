111beec970ab7212f53fc29a82035251b1446fb9
venustuspl <venustus.pl@gmail.com>
2020-02-02 10:47:12 +0100

    1.0.3 Founction findCountryPositiveAltitude

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,43 +7,55 @@ import java.util.*;
 
 public class Main {
 
+    public static Integer findCountryPositiveAltitude(Iterable<CSVRecord> records, String country) {
+        for (CSVRecord record : records) {
+
+            if (record.get(0).equals(country)) {
+                System.out.println(record.get(3));
+                return Integer.valueOf(record.get(3));
+
+            }
+        }
+        return 0;
+    }
+
 
     public static void main(String[] args) throws IOException {
 
-
         Reader in = new FileReader("happiness.csv");
-        String nazwaPliku = "result.txt";
+        String rusultFile = "result.txt";
 
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
         List<String> positiveCountryList = new ArrayList();
         Map<String, String> freedomMap = new TreeMap<>(Collections.reverseOrder());
         Map<String, String> corruptionMap = new TreeMap<>(Collections.reverseOrder());
+        Integer compareValue = Main.findCountryPositiveAltitude(records, "Poland");
         String trialRecord = "";
         for (CSVRecord record : records) {
 
             freedomMap.put(record.get(6), record.get(0));
             corruptionMap.put(record.get(7), record.get(0));
             trialRecord = record.get(3);
-            if (trialRecord.equals("")){
-                trialRecord="0";
+            if (trialRecord.equals("")) {
+                trialRecord = "0";
             }
-            if (Integer.valueOf(trialRecord) > 76) {
+            if (Integer.valueOf(trialRecord) > compareValue) {
                 positiveCountryList.add(record.get(0));
 
             }
         }
         try {
-            PrintWriter out = new PrintWriter(nazwaPliku);
-            out.append(" Pozytywne kraje: " + "\n");
+            PrintWriter out = new PrintWriter(rusultFile);
+            out.append("\n" + "1. Pozytywne kraje: " + "\n");
             for (String entry : positiveCountryList) {
                 out.append(entry + "\n");
             }
 
-            out.append(" Wolne kraje: " + "\n");
+            out.append("\n" + "2. Najbardziej wolnościowe kraje: " + "\n");
 
             int i = 0;
             for (Map.Entry<String, String> entry : freedomMap.entrySet()) {
-                out.append(entry.getValue() + entry.getKey() + "\n");
+                out.append(entry.getValue() + " " + entry.getKey() + "\n");
                 i++;
                 if (i == 5) {
                     i = 0;
@@ -51,10 +63,10 @@ public class Main {
 
                 }
             }
-            out.append(" Kraje o dużej korupcji: " + "\n");
+            out.append("\n" + "3. Kraje o największej korupcji: " + "\n");
 
             for (Map.Entry<String, String> entry : corruptionMap.entrySet()) {
-                out.append(entry.getValue() + entry.getKey() + "\n");
+                out.append(entry.getValue() + " " + entry.getKey() + "\n");
                 i++;
                 if (i == 5) {
                     i = 0;
@@ -64,7 +76,7 @@ public class Main {
             }
             out.close();
         } catch (FileNotFoundException ex) {
-            System.out.println("Niestety, nie mogę utworzyć pliku!");
+            System.out.println("Niestety, nie mogłem utworzyć pliku wyjściowego!");
         }
 
     }
